0a9358067c40582e4a44ad8e426f2bd14df81301
Sharma Bilaval <sharmb9@mcmaster.ca>
2018-11-10 23:00:47 -0500

    Final changes for search.html

--- javascript.js
+++ javascript.js
@@ -26,38 +26,33 @@ function validateForm() {
     // Initialize and add the map
     function initMap() {
     // The location of coords
-    var coords = {lat: 43.257921, lng: -79.9158836};
+    var coords = {lat: 43.263338, lng: -79.918282}; 
     // The map, centered at coords
     var map = new google.maps.Map(
         document.getElementById('result_map'), {zoom: 15, center: coords});
 
-    /*var marker = new google.maps.Marker({
-          position: coords,
-          map: map,
-          title: 'Hello World!'
-        }); */
-
     //Array of manually added parking spots with their coordinates
     var parkingSpots= [
-    ['Lot C', 43.265675, -79.915700],
-    ['Lot G', 43.266584, -79.918004],
-    ['Lot A', 43.262133, -79.917548],
+    ['Lot C', 'Rating: 3.5/5', 43.265675, -79.915700],
+    ['Lot G', 'Rating: 3/5', 43.266584, -79.918004],
+    ['Lot A', 'Rating: 4/5', 43.262133, -79.917548],
     ]
 
     // Adding markers and infor windows to parking spots
     var infoWindow= new google.maps.InfoWindow();
 
     var marker, i;
-
+    //Adding markers to the parking spots from the above array
     for (i = 0; i < parkingSpots.length; i++) {  
       marker = new google.maps.Marker({
-        position: new google.maps.LatLng(parkingSpots[i][1], parkingSpots[i][2]),
+        position: new google.maps.LatLng(parkingSpots[i][2], parkingSpots[i][3]),
         map: map
       });
 
+      //Pops up the info Window about the parking spot if the marker is clicked
       google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-          infoWindow.setContent(parkingSpots[i][0]);
+          infoWindow.setContent('<a href="parking.html">' + parkingSpots[i][0] + '</a>' + '<br>' + parkingSpots[i][1]);
           infoWindow.open(map, marker);
         }
       })(marker, i));
